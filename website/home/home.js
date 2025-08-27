0ef91ffcb3e7068741d027d310188bf8712886f9
krishal342 <krishal342@gmail.com>
2025-08-27 20:46:31 +0545

    Add files via upload

---
+++ website/home/home.js
@@ -0,0 +1,6 @@
+fetch('../header/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
