cb6610e88c3d6a1e88536d5909ded324638a137e
krishal342 <krishal342@gmail.com>
2025-08-27 21:29:52 +0545

    Delete contact directory

--- contact/contact.js
+++
@@ -1,6 +0,0 @@
-fetch('../header/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading header:', error));
