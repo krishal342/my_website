a3f8ec27f981dcbe0ff9d2c642f80b1d04711bac
krishal342 <krishal342@gmail.com>
2025-09-06 11:05:58 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-fetch('/project1/header/header.html')
+fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
         const script = document.createElement('script');
-        script.src = '/project1/header/header.js';
+        script.src = 'header.js';
         document.body.appendChild(script);
     })
     .catch(error => console.error('Error loading header:', error));
