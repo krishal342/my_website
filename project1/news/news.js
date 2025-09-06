da0ed73d1f74339d8c4cd0c717fd7cb334419f0a
krishal342 <krishal342@gmail.com>
2025-09-06 11:01:02 +0545

    Delete project1 directory

--- project1/news/news.js
+++
@@ -1,9 +0,0 @@
-fetch('/project1/header/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-        const script = document.createElement('script');
-        script.src = '/project1/header/header.js';
-        document.body.appendChild(script);
-    })
-    .catch(error => console.error('Error loading header:', error));
